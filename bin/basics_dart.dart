16a5a0714fb7d633ac2103c8814c1e8372f0a3dd
Tariqul Islam <islamtariqul565@gmail.com>
2023-02-15 03:47:52 +0600

    package method added

--- bin/basics_dart.dart
+++ bin/basics_dart.dart
@@ -1,5 +1,5 @@
-import 'package:basics_dart/basics_dart.dart' as basics_dart;
+import 'package:basics_dart/basics_dart.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${basics_dart.calculate()}!');
+void main() {
+  print('Hello world: ${calculateAdd()}!');
 }
--- lib/basics_dart.dart
+++ lib/basics_dart.dart
@@ -1,3 +1,9 @@
-int calculate() {
-  return 6 * 7;
+import 'package:calculator/calculator.dart';
+
+int calculateAdd() {
+  return add(24, 6);
+}
+
+int calculateSubtract() {
+  return subtract(5, 2);
 }
